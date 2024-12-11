86e41ec9e84402783621be9bf870074396969a21
Sk Soyeb Akhter <sksoyebakhter2003@gmail.com>
2024-12-11 15:07:24 +0530

    removed side panel permission

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -1,81 +1,84 @@
 import fs from 'node:fs';
-import deepmerge from 'deepmerge';
+// import deepmerge from 'deepmerge';
 
 const packageJson = JSON.parse(fs.readFileSync('../package.json', 'utf8'));
 
-const isFirefox = process.env.__FIREFOX__ === 'true';
+// const isFirefox = process.env.__FIREFOX__ === 'true';
 
-const sidePanelConfig = {
-  side_panel: {
-    default_path: 'side-panel/index.html',
-  },
-  permissions: ['sidePanel'],
-};
+// function withSidePanel(manifest) {
+//   // Firefox does not support sidePanel
+//   if (isFirefox) {
+//     return manifest;
+//   }
+//   return deepmerge(manifest, {
+//     side_panel: {
+//       default_path: 'side-panel/index.html',
+//     },
+//     permissions: ['sidePanel'],
+//   });
+// }
 
 /**
  * After changing, please reload the extension at `chrome://extensions`
  * @type {chrome.runtime.ManifestV3}
  */
-const manifest = deepmerge(
-  {
-    manifest_version: 3,
-    default_locale: 'en',
-    /**
-     * if you want to support multiple languages, you can use the following reference
-     * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Internationalization
-     */
-    name: '__MSG_extensionName__',
-    version: packageJson.version,
-    description: '__MSG_extensionDescription__',
-    // host_permissions: ['<all_urls>'],
-    permissions: ['activeTab', 'scripting'],
-    // options_page: 'options/index.html',
-    background: {
-      service_worker: 'background.iife.js',
-      type: 'module',
-    },
-    action: {
-      default_popup: 'popup/index.html',
-      default_icon: {
-        16: 'assets/icons/icon-16.png',
-        32: 'assets/icons/icon-32.png',
-        48: 'assets/icons/icon-48.png',
-        128: 'assets/icons/icon-128.png',
-      },
-    },
-    // chrome_url_overrides: {
-    //   newtab: 'new-tab/index.html',
-    // },
-    icons: {
+const manifest = {
+  manifest_version: 3,
+  default_locale: 'en',
+  /**
+   * if you want to support multiple languages, you can use the following reference
+   * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Internationalization
+   */
+  name: '__MSG_extensionName__',
+  version: packageJson.version,
+  description: '__MSG_extensionDescription__',
+  // host_permissions: ['<all_urls>'],
+  permissions: ['activeTab', 'scripting'],
+  // options_page: 'options/index.html',
+  background: {
+    service_worker: 'background.iife.js',
+    type: 'module',
+  },
+  action: {
+    default_popup: 'popup/index.html',
+    default_icon: {
       16: 'assets/icons/icon-16.png',
       32: 'assets/icons/icon-32.png',
       48: 'assets/icons/icon-48.png',
       128: 'assets/icons/icon-128.png',
     },
-    // content_scripts: [
-    //   {
-    //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-    //     js: ['content/index.iife.js'],
-    //   },
-    //   {
-    //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-    //     js: ['content-ui/index.iife.js'],
-    //   },
-    //   {
-    //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-    //     js: ['refresh.js'], // for public's HMR(refresh) support
-    //     css: ['styles/content.css'], // public folder
-    //   },
-    // ],
-    // devtools_page: 'devtools/index.html',
-    // web_accessible_resources: [
-    //   {
-    //     resources: ['*.js', '*.css', '*.svg', '*.png'],
-    //     matches: ['*://*/*'],
-    //   },
-    // ],
   },
-  !isFirefox && sidePanelConfig,
-);
+  // chrome_url_overrides: {
+  //   newtab: 'new-tab/index.html',
+  // },
+  icons: {
+    16: 'assets/icons/icon-16.png',
+    32: 'assets/icons/icon-32.png',
+    48: 'assets/icons/icon-48.png',
+    128: 'assets/icons/icon-128.png',
+  },
+  // content_scripts: [
+  //   {
+  //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+  //     js: ['content/index.iife.js'],
+  //   },
+  //   {
+  //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+  //     js: ['content-ui/index.iife.js'],
+  //   },
+  //   {
+  //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+  //     js: ['refresh.js'], // for public's HMR(refresh) support
+  //     css: ['styles/content.css'], // public folder
+  //   },
+  // ],
+  // devtools_page: 'devtools/index.html',
+  // web_accessible_resources: [
+  //   {
+  //     resources: ['*.js', '*.css', '*.svg', '*.png'],
+  //     matches: ['*://*/*'],
+  //   },
+  // ],
+};
 
 export default manifest;
